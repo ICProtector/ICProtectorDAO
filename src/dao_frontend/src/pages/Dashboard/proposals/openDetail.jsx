b6ff9d8d334c79b2937d303acb9e2e3b66a1f10b
Ubaid-developer <ubaidfsd234@gmail.com>
2024-07-04 15:44:55 +0500

    push

--- src/dao_frontend/src/pages/Dashboard/proposals/openDetail.jsx
+++ src/dao_frontend/src/pages/Dashboard/proposals/openDetail.jsx
@@ -321,11 +321,11 @@ const OpenDetails = (props) => {
                                 <div className="p-6 md:p-6 space-y-6">
                                     <form className="space-y-6" action="#" onSubmit={GivePoint}>
                                         <div>
-                                            <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                                            <input type="number" name="price" id="price"
+                                            <label htmlFor="price" className="text-start block mb-2 text-sm font-medium text-gray-900 dark:text-white">Points</label>
+                                            <input type="number" name="points" id="points"
                                                 value={points}
                                                 onChange={handleChange}
-                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter the price" required />
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter the points" required />
                                         </div>
 
                                         <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{isSubmitting ? "Giving..." : "Give Points"}</button>
